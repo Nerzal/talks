2e09241278bc8c72ebfe2d0cf3452477df791d2b
Ilija Afanasyev <ilija02@web.de>
2021-03-28 15:01:56 +0200

    fix input of WriteServerText to byte array

--- websockets/code/send.go
+++ websockets/code/send.go
@@ -1,6 +1,6 @@
 func send(conn net.Conn, message string) {
 	msg := "new server message"
-	err := wsutil.WriteServerText(conn, message)
+	err := wsutil.WriteServerText(conn, []byte(message))
 	if err != nil {
 		// handle error
 	}

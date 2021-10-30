6fd74653310453dbef0a84e48f6cb3b6ff21b489
Tobias Theel <tt@fino.digital>
2021-10-30 20:57:14 +0200

    yep

--- tinygo-iot-embeddedfest/code/iot/main.go
+++ tinygo-iot-embeddedfest/code/iot/main.go
@@ -1,9 +1,15 @@
 package main
 
 import ( // OMIT
-	"time" // OMIT
+	"device/arm" // OMIT
+	"machine"    // OMIT
+	"time"       // OMIT
 
-	"github.com/Nerzal/talks/tinygo-iot-embeddedfest/code/iot/mqttclient"
+	"github.com/Nerzal/talks/tinygo-iot-embeddedfest/code/iot/mqttclient" // OMIT
+	"github.com/Nerzal/talks/tinygo-iot-embeddedfest/code/iot/wifi"       // OMIT
+
+	"tinygo.org/x/drivers/lsm6ds3"  // OMIT
+	"tinygo.org/x/drivers/net/mqtt" // OMIT
 ) // OMIT
 // OMIT
 func main() {
@@ -20,3 +26,25 @@ func main() {
 		time.Sleep(time.Second)
 	}
 }
+
+// OMIT
+func InitializeSensor() lsm6ds3.Device { // OMIT
+	machine.I2C0.Configure(machine.I2CConfig{}) // OMIT
+	return lsm6ds3.New(machine.I2C0)            // OMIT
+} // OMIT
+// OMIT
+func InitializeMQTTClient() mqtt.Client { // OMIT
+	wifi.Setup()       // OMIT
+	wifi.ConnectToAP() // OMIT
+	// OMIT
+	client := mqttclient.NewClient() // OMIT
+	// OMIT
+	err := mqttclient.Connect(client) // OMIT
+	if err != nil {                   // OMIT
+		println(err.Error())    // OMIT
+		time.Sleep(time.Second) // OMIT
+		arm.SystemReset()       // OMIT
+	} // OMIT
+	// OMIT
+	return client // OMIT
+} // OMIT

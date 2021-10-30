3c2afca9c0f843a6cc7b7e6b5c63312f9eb2bd98
Tobias Theel <tt@fino.digital>
2021-10-30 21:15:51 +0200

    jub

--- tinygo-iot-embeddedfest/code/iot/mqttclient/publish.go
+++ tinygo-iot-embeddedfest/code/iot/mqttclient/publish.go
@@ -12,7 +12,7 @@ import ( // OMIT
 // OMIT
 func PublishMessage(client mqtt.Client, temp int32) {
 	println("sending message") // OMIT
-	token := client.Publish("embeddedfest/tinygo/iot/temperature", 2, true, strconv.Itoa(temp))
+	token := client.Publish("embeddedfest/tinygo/iot/temperature", 2, true, strconv.Itoa(int(temp)))
 	token.Wait()
 
 	err := token.Error()
